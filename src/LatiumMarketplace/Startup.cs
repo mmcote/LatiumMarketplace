7174007fb6c9a1997fc35d975348127a3a0cdfea
mmcote <mmcote@ualberta.ca>
2017-02-12 15:57:08 -0700

    Facebook login implemented

--- src/LatiumMarketplace/Startup.cs
+++ src/LatiumMarketplace/Startup.cs
@@ -57,6 +57,13 @@ namespace LatiumMarketplace
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            app.UseFacebookAuthentication(new FacebookOptions()
+            {
+                AppId = Configuration["Authentication:Facebook:AppId"],
+                AppSecret = Configuration["Authentication:Facebook:AppSecret"]
+            });
+
+
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
