7f2f72b9fccf487ce27244c9256b5640cac3cbad
mmcote <mmcote@ualberta.ca>
2017-02-22 11:30:31 -0700

    Basic skeleton on unit testing.

---
+++ AccountTests/AccountTestsTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using LatiumMarketplace;
+
+
+namespace AccountTests
+{
+    public class AccountTestsTests
+    {
+        [Fact]
+        public void ThingGetsObjectValFromNumber()
+        {
+            Assert.Equal(42, 42);
+        }
+    }
+}
