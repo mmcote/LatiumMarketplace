c796c0eb441d23e308e2cb5afa95c02124d45db5
Victoria Lee <Victoria Lee>
2017-02-24 10:36:18 -0800

    skeleton profile

--- src/LatiumMarketplace/Models/ApplicationUser.cs
+++ src/LatiumMarketplace/Models/ApplicationUser.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace LatiumMarketplace.Models
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        [Display(Name = "First Name")]
         public string firstName { get; set; }
+        [Display(Name = "Last Name")]
         public string lastName { get; set; }
+        [Display(Name = "Description")]
+        public string description { get; set; }
     }
 }
