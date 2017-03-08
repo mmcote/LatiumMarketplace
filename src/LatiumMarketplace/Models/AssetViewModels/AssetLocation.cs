3dd26d6c4d351078b31b80bf55592b1ad3e12b56
phlam1 <phlam1@ualberta.ca>
2017-03-08 16:25:41 -0700

    search by location works, ugly, but it works

---
+++ src/LatiumMarketplace/Models/AssetViewModels/AssetLocation.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LatiumMarketplace.Models.AssetViewModels
+{
+    public class AssetLocation
+    {
+        public List<Asset> assets;
+        public SelectList locations;
+        public string assetLocation { get; set; }
+    }
+}
