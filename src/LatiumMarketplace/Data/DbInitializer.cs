88de9c60b40e60ebfc196944784a769e1c71816b
Ape-with-a-keyboard <wankinvi@ualberta.ca>
2017-03-10 03:11:13 -0700

    added in test data for asset, if you delete your database and recreate one, it will auto product 4 assets(2 request, 2 asset). Look at my template code in DbInitializer, you can add you own test data toogit add .

---
+++ src/LatiumMarketplace/Data/DbInitializer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using LatiumMarketplace.Models;
+using LatiumMarketplace.Models.AssetViewModels;
+
+namespace LatiumMarketplace.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(ApplicationDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any students.
+            if (context.Asset.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            var assets = new Asset[]
+            {
+            new Asset{addDate = DateTime.Parse("2005-09-01"),description="This will only work above 15 degree C", location="Edmonton, Alberta", name="Bobcat", ownerID="TestCode233", price=50000, priceDaily=180, priceMonthly=2500,priceWeekly=890, accessory="With Cover", request=false},
+             new Asset{addDate = DateTime.Parse("2015-09-01"),description="This will only work above 20 degree C", location="Calgary, Alberta", name="Tow Truck", ownerID="TestCode233", price=110000, priceDaily=400, priceMonthly=4000,priceWeekly=2800, accessory="Winter tires on with stud", request=false},
+             new Asset{addDate = DateTime.Parse("2017-01-01"),description="This will work above - 15 degree C", location="Edmonton, Alberta", name="Semi-16wheel", ownerID="TestCode233", accessory= null, request=true},
+             new Asset{addDate = DateTime.Parse("2017-03-01"),description="This will work above - 30 degree C", location="St.albert, Alberta", name="Semi-18wheel", ownerID="TestCode233", accessory= "Comes with self driving system", request=true},
+            };
+            foreach (Asset s in assets)
+            {
+                context.Asset.Add(s);
+            }
+            context.SaveChanges();
+        }
+    }
+}
--- src/LatiumMarketplace/Startup.cs
+++ src/LatiumMarketplace/Startup.cs
@@ -92,7 +92,7 @@ namespace LatiumMarketplace
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, ApplicationDbContext context)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
@@ -131,6 +131,7 @@ namespace LatiumMarketplace
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+            DbInitializer.Initialize(context);
         }
     }
 }
