33095883ad3ad3d2e6d5f6e3b20d3c00635d31c5
leevtori <vlee2@ualberta.ca>
2017-03-17 15:17:16 -0700

    added the migration, should work, please test

--- src/LatiumMarketplace/Data/Migrations/20170317183437_duh.Designer.cs
+++ src/LatiumMarketplace/Data/Migrations/20170317183437_duh.Designer.cs
@@ -8,9 +8,10 @@ using LatiumMarketplace.Data;
 namespace LatiumMarketplace.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170317183437_duh")]
+    partial class duh
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.1")
