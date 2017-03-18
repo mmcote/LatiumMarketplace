70ad0f56f29b244446278207e8f5341884e6001a
ookmm <gaylordmbuyi@gmail.com>
2017-03-17 22:49:42 -0600

    Added toggle to display price based on for sale or for rent.

--- src/LatiumMarketplace/wwwroot/js/site.js
+++ src/LatiumMarketplace/wwwroot/js/site.js
@@ -1,5 +1,13 @@
 ﻿// Write your Javascript code.
 
+
+// Show rates based on radio button selection
+// If asset is for sale or for rent
 $(document).ready(function () {
-    
+    $("input[name=assetCreateOptions]").click(function () {
+        var inputValue = $(this).attr("value");
+        var targetElement = $("#" + inputValue);
+        $(".asset-rates").not(targetElement).hide();
+        $(targetElement).show();
+    });
 });
