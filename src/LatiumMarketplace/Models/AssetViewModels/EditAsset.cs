de37fdbc3c91238b7911000e8119d05d5e9dc75f
Michael Cote <mmcote@ualberta.ca>
2017-04-01 14:30:40 -0600

    Fixed dropdowns in edit asset, Send message to specific user in admin

---
+++ src/LatiumMarketplace/Models/AssetViewModels/EditAsset.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LatiumMarketplace.Models.AssetViewModels
+{
+    public class EditAsset
+    {
+        public EditAsset(Asset asset, IEnumerable<Make> makes)
+        {
+            editableAsset = asset;
+            Makes = makes;
+        }
+        public Asset editableAsset { get; set; }
+        public IEnumerable<Make> Makes { get; set; }
+    }
+}
