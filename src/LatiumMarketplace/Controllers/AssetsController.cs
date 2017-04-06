9a7ab972c88198d94bd0983b1ce47d553c535d10
leevtori <vlee2@ualberta.ca>
2017-04-06 10:44:55 -0700

    minor

--- src/LatiumMarketplace/Controllers/AssetsController.cs
+++ src/LatiumMarketplace/Controllers/AssetsController.cs
@@ -102,62 +102,6 @@ namespace LatiumMarketplace.Controllers
             return View(assetLocatioinVM);
         }
 
-        /*
-        // GET: Assets
-        [AllowAnonymous]
-        public async Task<IActionResult> Index(string assetLocation, string searchString, string sortby, bool recent, bool accessory)
-        {
-            // Use LINQ to get list of genres.
-            IQueryable<string> locationQuery = from m in _context.Asset
-                                               orderby m.location
-                                               select m.location;
-            var assets = from m in _context.Asset
-                         select m;
-            switch (sortby)
-            {
-                case "request":
-                    if (accessory == true)
-                    {
-                        assets = assets.Where(s => s.accessory != null);
-                    }
-                    assets = assets.Where(s => s.request.Equals(true));
-                    break;
-                case "asset":
-                    if (accessory == true)
-                    {
-                        assets = assets.Where(s => s.accessory != null);
-                    }
-                    assets = assets.Where(s => s.request.Equals(false));
-                    break;
-                case "all":
-                    assets = from m in _context.Asset
-                             select m;
-                    if (accessory == true)
-                    {
-                        assets = assets.Where(s => s.accessory != null);
-                    }
-                    break;
-            }
-            if (recent == true)
-            {
-                assets = assets.OrderByDescending(s => s.addDate);
-            }
-            if (!String.IsNullOrEmpty(assetLocation))
-            {
-                assets = assets.Where(x => x.location == assetLocation);
-            }
-            if (!String.IsNullOrEmpty(searchString))
-            {
-                assets = assets.Where(x => x.name.Contains(searchString));
-            }
-            var assetLocatioinVM = new AssetLocation();
-            assetLocatioinVM.locations = new SelectList(await locationQuery.Distinct().ToListAsync());
-            assetLocatioinVM.assets = await assets.ToListAsync();
-            return View(assetLocatioinVM);
-            //return View(await assets.ToListAsync());
-        }
-        */
-
         /*============================ */
         
       
